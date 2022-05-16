37cf12b0df3743e4d547c491b24a5dd77850958e
Hossen <mdmitho199911@gmail.com>
2022-05-17 01:38:46 +0600

    VerifyAdmin middleware and load all doctors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const cors =require('cors')
-const jwt = require('jsonwebtoken')
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const app = express()
-const port = process.env.PROT || 5000
+const app = express();
+const port = process.env.PROT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nwhtf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -22,7 +22,7 @@ function verifyJWT(req, res, next) {
   if (!authHeader) {
     return res.status(401).send({ message: "UnAuthorized access" });
   }
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
     if (err) {
       return res.status(403).send({ message: "Forbidden access" });
@@ -40,59 +40,58 @@ async function run() {
     const userCollection = client.db("doctors-portal").collection("users");
     const doctorCollection = client.db("doctors-portal").collection("doctors");
 
+    const verifyAdmin = async (req, ser, next) => {
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({ email: requester });
+      if (requesterAccount.role === "admin") {
+        next();
+      } else res.status(403).send({ message: "forbidden" });
+    };
+
     app.get("/service", async (req, res) => {
       const query = {};
-      const cursor = serviceCollection.find(query).project({name:1});
+      const cursor = serviceCollection.find(query).project({ name: 1 });
       const services = await cursor.toArray();
       res.send(services);
     });
 
-    app.get('/user', async(req,res)=>{
-      const users = await userCollection.find().toArray()
-      res.send(users)
-    })
-
-    app.get('/admin/:email', async(req,res)=>{
-      const email =req.params.email
-      const user = await userCollection.findOne({email:email})
-      const isAdmin = user.role === 'admin'
-      res.send({admin:isAdmin})
-    })
-
-
-    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
-      const email = req.params.email;
-      const requester =  req.decoded.email
-      const  requesterAccount = await userCollection.findOne({email:requester})
-      if(requesterAccount.role === 'admin'){
-        const filter = { email: email };
-        const updateDoc = {
-          $set: {role:'admin'},
-        };
-        const result = await userCollection.updateOne(filter, updateDoc);
-        res.send(result);
-      }
-      else(
-        res.status(403).send({message:'forbidden'})
-      )
-     
+    app.get("/user", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
 
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-    app.put('/user/:email',async (req, res)=>{
+    app.put("/user/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
-      const filter = {email:email}
-      const options = {upsert:true};
-      const updateDoc ={
-        $set:user,
-      }
-      const result = await userCollection.updateOne(filter, updateDoc,options)
-      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
 
-      res.send({result,token})
-    })
+      res.send({ result, token });
+    });
 
     /****
      * API Naming Convention
@@ -135,28 +134,22 @@ async function run() {
         service.slots = available;
       });
 
-
-
-  
-// console.log(bookings);
+      // console.log(bookings);
       // console.log(services);
       res.send(services);
     });
 
-
-    app.get('/booking', verifyJWT, async (req, res) => {
+    app.get("/booking", verifyJWT, async (req, res) => {
       const patient = req.query.patient;
       const decodedEmail = req.decoded.email;
       if (patient === decodedEmail) {
         const query = { patient: patient };
         const bookings = await bookingCollection.find(query).toArray();
         return res.send(bookings);
+      } else {
+        return res.status(403).send({ message: "forbidden access" });
       }
-      else {
-        return res.status(403).send({ message: 'forbidden access' });
-      }
-    })
-
+    });
 
     app.post("/booking", async (req, res) => {
       const booking = req.body;
@@ -173,31 +166,27 @@ async function run() {
       return res.send({ success: true, result });
     });
 
+    app.get('/doctor',verifyJWT,verifyAdmin,async(req,res)=>{
+
+      const doctors = await doctorCollection.find().toArray()
+      res.send(doctors)
 
 
-app.post('/doctor',async(req,res)=>{
-const doctor =req.body;
-const result= await doctorCollection.insertOne(doctor)
-res.send(result)
-})
+    })
 
-
-
-
-
-
-
-  }
-  finally{
-
+    app.post("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctor = req.body;
+      const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    });
+  } finally {
   }
 }
-run().catch(console.dir)
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello my website')
-})
+app.get("/", (req, res) => {
+  res.send("Hello my website");
+});
 
 app.listen(port, () => {
   console.log(`my app listening on port ${port}`);

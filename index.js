4e30fa6f653245618528667319f1c68310f68e9e
Hossen <mdmitho199911@gmail.com>
2022-05-17 00:48:33 +0600

    Save Doctor info in the database and display success message

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ async function run() {
     const serviceCollection = client.db("doctors-portal").collection("services");
     const bookingCollection = client.db("doctors-portal").collection("bookings");
     const userCollection = client.db("doctors-portal").collection("users");
+    const doctorCollection = client.db("doctors-portal").collection("doctors");
 
     app.get("/service", async (req, res) => {
       const query = {};
@@ -171,6 +172,21 @@ async function run() {
       const result = await bookingCollection.insertOne(booking);
       return res.send({ success: true, result });
     });
+
+
+
+app.post('/doctor',async(req,res)=>{
+const doctor =req.body;
+const result= await doctorCollection.insertOne(doctor)
+res.send(result)
+})
+
+
+
+
+
+
+
   }
   finally{
 
